b4409278d0c6c93ac28b6f23f58124f1376f2293
lin2101 <1054742101@qq.com>
2025-12-25 14:03:08 +0800

    feat: 添加 本地测试与netlify 的 npm包区分

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import cesium from 'vite-plugin-cesium'
+import path from 'path'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   base: './',
   plugins: [cesium()],
   server: {
     port: 3000,
   },
-})
+  resolve: {
+    alias: mode === 'development'
+      ? {
+          'cesium-transform-controls': path.resolve(__dirname, '../src/index.ts'),
+        }
+      : undefined,//使用 npm 包（用于 netlify 部署）
+  },
+}))
+
