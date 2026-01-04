8e050dc077d8ac67288efc9c7ec5961ab41f1564
lin2101 <1054742101@qq.com>
2026-01-04 13:47:42 +0800

    pref:优化example示例

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -9,11 +9,9 @@ export default defineConfig(({ mode }) => ({
     port: 3000,
   },
   resolve: {
-    alias: mode === 'development'
-      ? {
-          'cesium-transform-controls': path.resolve(__dirname, '../src/index.ts'),
-        }
-      : undefined,//使用 npm 包（用于 netlify 部署）
+    alias: {
+      'cesium-transform-controls': path.resolve(__dirname, '../src/index.ts'),
+    },
   },
 }))
 
